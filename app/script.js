60b5d2facdb57b36b6d6454447d59acc94e5e29f
Feifei Yang <feifeiyang@feifeis-air.home>
2021-10-08 14:56:24 -0400

    image

---
+++ app/script.js
@@ -0,0 +1,9 @@
+const button1 = document.getElementById('button1')
+let audio = new Audio();
+
+
+
+
+button1.addEventListener('click', function(){
+    console.log('click');
+}) 
--- script.js
+++
@@ -1,4 +0,0 @@
-const button1 = document.getElementById('button1')
-button1.addEventListener('click', function(){
-    console.log('click');
-})
